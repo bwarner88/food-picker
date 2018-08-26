0cafde6d6810bc8d81ea1abdb4cdcb5c34317478
unknown <brian_warner25@yahoo.com>
2018-08-25 22:35:15 -0700

    clicks and random

---
+++ assets/app.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+
+    const placesToEat = [];
+
+
+    var onClick = function () {
+        $('#submit').on('click', function () {
+            var userInput = $('#inputField').val();
+            $('.choicesField').append('<ul>' + '<li>' + userInput + '</li>' + '</ul>');
+            $('#inputField').val('')
+            placesToEat.push(userInput)
+
+        });
+    }
+
+    var foodRandom = function() {
+    $('.pick').on('click', function () {
+        var yourDinner = placesToEat[Math.floor(Math.random() * placesToEat.length)]
+
+        $('.resultsField').append(yourDinner)
+
+    })
+}
+
+onClick();
+foodRandom();
+
+});
