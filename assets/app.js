7a5ab96e2e856609b83ca3eaaa50c9bfa939f0c6
unknown <brian_warner25@yahoo.com>
2018-10-01 15:15:44 -0700

    added reset functionality

--- assets/app.js
+++ assets/app.js
@@ -1,28 +1,40 @@
-$(document).ready(function () {
+$(document).ready(() => {
 
     const placesToEat = [];
 
 
-    var onClick = function () {
+    const onClick = () => {
        
-        $('#submit').on('click', function (event) {
+        $('#submit').on('click', (event) => {
             event.preventDefault();
-            var userInput = $('#inputField').val();
+            const userInput = $('#inputField').val();
             $('.choicesField').append('<ul>' + '<li>' + userInput + '</li>' + '</ul>');
             $('#inputField').val('')
             placesToEat.push(userInput)
 
         });
+
+        $('.btn btn-secondary').on('click', event => {
+            event.preventDefault();
+            reset();
+        })
     }
 
-    var foodRandom = function () {
-        $('.pick').on('click', function () {
-            var yourDinner = placesToEat[Math.floor(Math.random() * placesToEat.length)]
+    const foodRandom = () => {
+        $('.pick').on('click', () => {
+            const yourDinner = placesToEat[Math.floor(Math.random() * placesToEat.length)]
 
             $('.resultsField').append(yourDinner)
 
         })
     }
+
+    const reset = () => {
+        const placesToEat = [];
+        const yourDinner = "";
+    };
+
+
 
     onClick();
     foodRandom();
